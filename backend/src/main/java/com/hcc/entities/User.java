618a44e445f075db27a31c7dee13de3fd5f2af06
Tom Tarpey <tomtarpeydev@gmail.com>
2023-01-04 04:09:48 +0000

    added spring security and some boilerplate to override the defaults

--- backend/src/main/java/com/hcc/entities/User.java
+++ backend/src/main/java/com/hcc/entities/User.java
@@ -1,12 +1,17 @@
 package com.hcc.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -48,6 +53,35 @@ public class User {
         this.cohortStartDate = cohortStartDate;
     }
 
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new Authority("role_student"));
+        return roles;
+    }
+
+    @Override
     public String getUsername() {
         return username;
     }
@@ -56,6 +90,7 @@ public class User {
         this.username = username;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
---
+++ backend/src/main/java/com/hcc/services/UserDetailServiceImpl.java
@@ -0,0 +1,23 @@
+package com.hcc.services;
+
+import com.hcc.entities.User;
+import com.hcc.utils.CustomPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    CustomPasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.getPasswordEncoder().encode("asdfasdf"));
+        return user;
+    }
+}
