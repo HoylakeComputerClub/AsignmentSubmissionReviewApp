bf196d5b9e1e10d7820c3f00ce6e4238662a884b
Tom Tarpey <tomtarpeydev@gmail.com>
2023-01-05 16:24:03 +0000

    added custom hook for localstorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
-import axios from 'axios';
 import './App.css';
-
+import { useEffect } from 'react';
+import { useLocalState } from './utils/useLocalStorage';
 function App() {
-  axios.post("http://localhost:8080/api/auth/login", {"username": "tom2", "password": "asdfasdf"})
-  .then((res) => console.log(res));
+
+  const [jwt, setJwt] = useLocalState("", "jwt");
+
+  useEffect(() => {
+    if (!jwt) {
+      
+      const reqBody = {"username": "tom2", "password": "asdfasdf"};
+
+      fetch("api/auth/login",{"headers": { "Content-Type": "application/json"}, 
+      method: "post", body: JSON.stringify(reqBody)}).then((res) => Promise.all([res, res.headers]))
+      .then(([body, headers]) => {
+        setJwt(headers.get("authorization"));
+      });
+    }
+  }, [jwt, setJwt]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Assignment Submission Review App</h1>
+        <h2>JWT</h2>
+        <p>{jwt}</p>
       </header>
     </div>
   );
