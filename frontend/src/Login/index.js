0020c183b5059556ce0be359d6d976c244b28a11
Tom Tarpey <tomtarpeydev@gmail.com>
2023-01-07 14:00:49 +0000

    added bootstrap and react-bootstrap in prep for the styles. and changed the login divs to be react-bootstrap components

--- frontend/src/Login/index.js
+++ frontend/src/Login/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import fetcher from '../Services/fetchService';
 import { useLocalState } from '../utils/useLocalStorage';
+import { Button, Container, Form } from 'react-bootstrap';
+import "./Login.css";
 
 
 const Login = (props) => {
@@ -8,53 +9,45 @@ const Login = (props) => {
     const [password, setPassword] = useState("");
     const [jwt, setJwt] = useLocalState("", "jwt");
     function sendLoginRequest () {
-        //     console.log("login submit requested!");
-         
-        // const reqBody = {"username": username, "password": password};
-        // fetcher("api/auth/login", "post", jwt, reqBody).then(([body, headers]) => {
-        //     setJwt(headers.get("authorization"));
-        //     window.location.href = "dashboard";
-        //   })
-        //   .catch((message) => alert(message));
-            console.log("login submit requested!");
-         
-         const reqBody = {"username": username, "password": password};
-    
-          fetch("api/auth/login",{"headers": { "Content-Type": "application/json"}, 
-          method: "post", body: JSON.stringify(reqBody)}).then((res) => {
-            if (res.status === 200) {
-                return Promise.all([res, res.headers]);
-            } else {
-                return Promise.reject("Invalid Login Attempt!");
-            }
-        })
-          .then(([body, headers]) => {
-            setJwt(headers.get("authorization"));
-            window.location.href = "dashboard";
-          })
-          .catch((message) => alert(message));
+
+        console.log("login submit requested!");
+     
+     const reqBody = {"username": username, "password": password};
+
+      fetch("api/auth/login",{"headers": { "Content-Type": "application/json"}, 
+      method: "post", body: JSON.stringify(reqBody)}).then((res) => {
+        if (res.status === 200) {
+            return Promise.all([res, res.headers]);
+        } else {
+            return Promise.reject("Invalid Login Attempt!");
         }
+    })
+      .then(([body, headers]) => {
+        setJwt(headers.get("authorization"));
+        window.location.href = "dashboard";
+      })
+      .catch((message) => alert(message));
+    }
     return (
         <>
-            <div>
+            <Container>
                 <h1>Login Page!!!</h1>
                 <p>{props.jwt}</p>
-                <div>
-                    <div>
-                        <label htmlFor='username'>Username</label>
-                        <input type='email' id='username' value={username} onChange={(e) => setUsername(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor='password'>Password</label>
-                        <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                </div>
-            </div>
-            <div>
-                <button id='submit' type='button' onClick={() => sendLoginRequest()} >
+                <Form.Group>
+                    <Form.Group className="formItem">
+                        <Form.Label htmlFor='username'>Username</Form.Label>
+                        <Form.Control type='email' id='username' placeholder='Enter Your Username here...' value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="formItem">
+                        <Form.Label htmlFor='password'>Password</Form.Label>
+                        <Form.Control type='password' id='password' placeholder='Enter Your Password here...' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </Form.Group>
+                </Form.Group>
+                <Button className="formItem" id='submit' type='button' onClick={() => sendLoginRequest()} >
                     Login
-                </button>
-            </div>
+                </Button>
+
+            </Container>
         </>
     )
 }
