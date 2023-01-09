5631633693eb7e4f11e38dd35fc0004b17ec9386
Tom Tarpey <tomtarpeydev@gmail.com>
2023-01-09 17:31:07 +0000

    added logout and go to dhashboard buttons to homepage

--- frontend/src/HomePage/index.js
+++ frontend/src/HomePage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const HomePage = (props) => {
@@ -6,6 +7,7 @@ const HomePage = (props) => {
         <div>
             <h1>Home Page!!!</h1>
             <p>{props.jwt}</p>
+            <button>{props.jwt ? <Link to="/dashboard">Go To your Dashboard</Link> : <Link to="/login">Login</Link>}</button>
         </div>
     )
 }
