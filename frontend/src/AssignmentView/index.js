fc10f9a56267e8ab95eca4e0984655270b14eaa5
Tom Tarpey <tomtarpeydev@gmail.com>
2023-01-10 17:23:46 +0000

    added some styles to the assignments furm and also added a dropdown control to facilitate a switch of assignments to edit in a future feature add

--- frontend/src/AssignmentView/index.js
+++ frontend/src/AssignmentView/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Badge, Button, Col, Dropdown, DropdownButton, Form, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import fetcher from '../Services/fetchService';
 import { useLocalState } from '../utils/useLocalStorage';
@@ -24,20 +24,58 @@ const AssignmentView = (props) => {
     
 
     return (
-        <div>
-            <h1>
-                Assignment {assignmentId}
-            </h1>
+        <div><Row className='my-5'>
+            <Col>
+                <h2>
+                    Assignment {assignmentId}
+                </h2>
+            </Col>
+            <Col>
+                <Badge style={{lineHeight: '1.6rem', marginTop: '5px', paddingLeft: '20px', paddingRight: '20px', fontSize: '1rem', fontWeight: '300'}}pill bg='success'>{assignment.status}</Badge>
+            </Col>
+        </Row>
             {assignment ? (
-                <>
-                    <h2>Status: {assignment.status}</h2>
-                    <h3>Assignment Name: <input type='name' id='name' onChange={(e) => {setAssignment({...assignment, "name": e.target.value}); console.log(assignment)}} value={assignment.name} /></h3>
-                    <h3>Github URL: <input type='url' id='githubUrl' onChange={(e) => {setAssignment({...assignment, "githubUrl": e.target.value}); console.log(assignment)}} value={assignment.githubUrl} /></h3>
-                    <h3>Branch: <input type='text' id='branch' onChange={(e) => {setAssignment({...assignment, "branch": e.target.value}); console.log(assignment)}} value={assignment.branch} /></h3>
+                <><Form.Group as={Row} className='my-5 justify-content-center'>
+                <Col md='10'>
+                <Form.Label>
+                    Assignment
+                </Form.Label>
+                <DropdownButton id='assignmentName' variant='success' title={assignment.name}>
+                    {['1', '2', '3', '4', '5', '6', '7', '8'].map(assignmentNum => <Dropdown.Item eventKey={assignmentNum}>{assignment.name + " " + assignmentNum}</Dropdown.Item>)}
+
+
+
+                </DropdownButton>
+
+                    <Form.Control type='dropdown' id='name' onChange={(e) => {setAssignment({...assignment, "name": e.target.value}); console.log(assignment)}} value={assignment.name} />
+                </Col>
+                </Form.Group>
+                <Form.Group as={Row} className='my-5 justify-content-center'>
+                    <Col md='10'>
+                    <Form.Label>
+                        Assignment Name
+                    </Form.Label>
+                        <Form.Control type='name' id='name' onChange={(e) => {setAssignment({...assignment, "name": e.target.value}); console.log(assignment)}} value={assignment.name} />
+                    </Col>
+                    <Col md='10'>
+                    <Form.Label>
+                        Github URL
+                    </Form.Label>
+                        <Form.Control type='url' id='githubUrl' onChange={(e) => {setAssignment({...assignment, "githubUrl": e.target.value}); console.log(assignment)}} value={assignment.githubUrl} />
+                    </Col>
+                    <Col md='10'>
+                    <Form.Label>
+                        Branch
+                    </Form.Label>
+                    <Form.Control type='text' id='branch' onChange={(e) => {setAssignment({...assignment, "branch": e.target.value}); console.log(assignment)}} value={assignment.branch} />
+                    </Col>
+                    <Col md='10'>
                     <Button variant='success' onClick={() => {setAssignment({...assignment, status: "ready for review"}); saveAssignment(); window.location.href = '/dashboard'}} className='m-3'>Submit Assignment</Button>
-                    <Button variant='secondary' onClick={() => window.location.href = '/dashboard'}>Back to Dashboard</Button>
-                </>
-            ) : (<></>)}
+                    <Button variant='secondary' onClick={() => window.location.href = '/dashboard'} className='m-3'>Back to Dashboard</Button>
+
+                    </Col>
+                </Form.Group>
+                </> ) : (<></>)}
         </div>
     );
 }
